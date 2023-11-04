6941e02656ef45db599b347438d8188d9a9232c3
anuva04 <anuvacotton2016@gmail.com>
2023-11-05 00:00:14 +0530

    Composite pattern

---
+++ DesignPatterns/StructuralPatterns/CompositePattern.java
@@ -0,0 +1,122 @@
+import java.util.*;
+import java.lang.UnsupportedOperationException;
+
+class CompositePattern {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        Folder SystemDesign = new Folder("SystemDesign", null);
+
+        Directory DesignPatterns = new Folder("DesignPatterns", SystemDesign);
+        System.out.println("Path of DesignPatterns:");
+        DesignPatterns.printPath();
+        System.out.println("");
+
+        Directory ElevatorSystem = new Folder("ElevatorSystem", SystemDesign);
+        System.out.println("Path of ElevatorSystem:");
+        ElevatorSystem.printPath();
+        System.out.println("");
+
+        Directory StructuralDesignPatterns = new Folder("StructuralDesignPatterns", DesignPatterns);
+        System.out.println("Path of StructuralDesignPatterns:");
+        StructuralDesignPatterns.printPath();
+        System.out.println("");
+
+        System.out.println("Subdirectories of DesignPatterns:");
+        DesignPatterns.printSubDirectories();
+        System.out.println("");
+
+        Directory CompositePattern = new File("CompositePattern", StructuralDesignPatterns);
+        System.out.println("Path of CompositePattern:");
+        CompositePattern.printPath();
+        System.out.println("");
+        System.out.println("Subdirectories of CompositePattern:");
+        CompositePattern.printSubDirectories();
+        System.out.println("");
+
+        System.out.println("Subdirectories of SystemDesign:");
+        SystemDesign.printSubDirectories();
+        System.out.println("");
+    }
+}
+
+interface Directory {
+    public void printPath();
+    public void printSubDirectories();
+    public void add(Directory directory);
+    public void remove(Directory directory);
+}
+
+// Composite element
+class Folder implements Directory {
+    public String name;
+    public Directory parent;
+    public List<Directory> subFolders;
+
+    public Folder(String name, Directory parent) {
+        this.name = name;
+        this.parent = parent;
+        this.subFolders = new ArrayList<>();
+        if(parent != null) parent.add(this);
+    }
+
+    @Override
+    public void printPath() {
+        if(parent != null) parent.printPath();
+        System.out.print("/" + this.name);
+    }
+
+    @Override
+    public void printSubDirectories() {
+        for(Directory child : this.subFolders) {
+            if(child instanceof Folder) {
+                Folder folder = (Folder) child;
+                System.out.println(folder.name);
+            } else {
+                File file = (File) child;
+                System.out.println(file.name);
+            }
+        }
+    }
+
+    @Override
+    public void add(Directory child) {
+        this.subFolders.add(child);
+    }
+
+    @Override
+    public void remove(Directory child) {
+        this.subFolders.remove(child);
+    }
+}
+
+// Leaf element
+class File implements Directory {
+    public String name;
+    public Directory parent;
+
+    public File(String name, Directory parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    @Override
+    public void printPath() {
+        if(parent != null) parent.printPath();
+        System.out.print("/" + this.name);
+    }
+
+    @Override
+    public void printSubDirectories() {
+        System.out.print("This is a file.");
+    }
+
+    @Override
+    public void add(Directory child) {
+        throw new UnsupportedOperationException("Can't add in a file.");
+    }
+
+    @Override
+    public void remove(Directory child) {
+        throw new UnsupportedOperationException("Can't remove from a file.");
+    }
+}
